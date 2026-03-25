44c2ea617e98f50f08da66b91f9bcaecd1c8dcc5
Qwertyzx1c <jakub.ura10@gmail.com>
2026-03-25 13:12:33 +0100

    Update FUTURIT.js

--- FUTURIT.js
+++ FUTURIT.js
@@ -1,24 +1,33 @@
 const oblicz = document.querySelector("button")
+const kursy = document.querySelectorAll("input[type='checkbox']")
 
 function funkcja_obliczeniowa(event){
     event.preventDefault()
     
-    const checkboxy = document.querySelectorAll("input[type='checkbox']:checked")
-    const miasto = document.querySelector("#miasta").value
-    
-    const raty = document.querySelector("#ratatuj").value
-
     const tabela = document.querySelector("table")
+    const ilosc_rat = document.querySelector("#raty").value
+    let suma = 0
     
     tabela.innerHTML = `<tr>
-        <td>Kurs</td>
-        <td>Kwota</td>
-    </tr`
-    for(let kurs of checkboxy){
-        tabela.innerHTML += `<tr>
+        <td>Kurs:</td>
+        <td>Kwota:</td>
+    </tr>`
+
+    for(let kurs of kursy){
+        if(kurs.checked){
+            tabela.innerHTML += `<tr>
             <td>${kurs.name}</td>
             <td>${kurs.value}</td>
-        </tr>`
+            </tr>`
+
+            suma += Number(kurs.value)
+        }
     }
+
+    const raty_span = document.querySelector('#raty_span')
+    const suma_span = document.querySelector('#suma_span')
+
+    raty_span.textContent = ilosc_rat
+    suma_span.textContent = Number(suma / ilosc_rat).toFixed(2)
 }
 oblicz.addEventListener("click", funkcja_obliczeniowa)
